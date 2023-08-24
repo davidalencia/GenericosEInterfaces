792fdc5387dd22033bfd8ef3e475290376dd7cd2
ErikQuintero <erik_quintero@ciencias.unam.mx>
2023-08-23 21:07:38 -0600

    modificando la clase Tacos

--- Tacos.java
+++ Tacos.java
@@ -1,16 +1,34 @@
 
-public class Tacos implements Restaurante {
-  private double estrellas = 4.5;
+public class Tacos implements Restaurante, Comparable<Tacos>{
+  private double estrellas;
+  private int variedadesTacos;
+  private String nombre;
 
+  public Tacos (double estrellas, int variedadesTacos, String nombre){
+    this.estrellas = estrellas;
+    this.variedadesTacos = variedadesTacos;
+    this.nombre = nombre;
+  }
   public double getEstrellas() {
     return estrellas;
   }
 
   public void cocina(String platillo) {
-    System.out.println("Tacos: cocinando " + platillo);
+    System.out.println("Tacos: cocinando " + platillo + "con salsa o sin salsa?");
   }
 
   public void cobra(double cantidad) {
-    System.out.println("Tacos: cobrando: $" + cantidad);
+    System.out.println("Tacos: cobrando: $" + cantidad + "cuanto desea de propina");
+  }
+
+  public int compareTo(Tacos tac){
+    if(this.variedadesTacos != tac.variedadesTacos){
+      return this.variedadesTacos - tac.variedadesTacos;
+    }
+    return 0;
+  }
+
+  public String toString(){
+    return this.nombre;
   }
 }
--- Uber.java
+++ Uber.java
@@ -1,10 +1,10 @@
 public class Uber {
-  //Restaurante[] restos = { new Tacos(), new Sopes() };
+  Restaurante[] restos = { new Tacos(4.5, 5, "Taqueria Champion"), new Sopes("Doña Chona", 3) };
 
   public void preparaPlatillos() {
     System.out.println("preparando platillos");
-    //for (Restaurante res : restos){
-      //res.cocina("quesadilla");
-      //}
+    for (Restaurante res : restos){
+      res.cocina("quesadilla");
+    }
   }
 }
